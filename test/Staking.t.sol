d2a84ac0cf396c6ad8234b3b7ff9ed553f075af7
Togloza <120382975+Togloza@users.noreply.github.com>
2023-06-20 19:26:17 -0700

    Working setUp function test

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -6,13 +6,20 @@ import "forge-std/console.sol";
 import "../src/Staking.sol";
 import "../src/WindfallFactory.sol";
 import "../src/WinToken.sol";
+import "../src/Access.sol";
 
 contract WindfallTest is Test {
 
-
+    WindfallFactory public factory;
+    Access public access;
+    WinToken public wintoken;
+    Staking public staking;
 
     function setUp() public {
-
+        factory = new WindfallFactory();
+        access = Access(factory.access());
+        wintoken = WinToken(factory.wintoken());
+        staking = Staking(factory.staking());
     }
 
 
